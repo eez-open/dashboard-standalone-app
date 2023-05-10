3060be53fd05f608cede9853fcd6216e89d38c5a
Martin Vladic <martin.vladic@gmail.com>
2023-05-10 16:56:11 +0200

    icon changed

--- packages/standalone/renderer.tsx
+++ packages/standalone/renderer.tsx
@@ -129,7 +129,6 @@ export class ProjectEditorTab {
                 return;
             }
 
-            await projectEditorStore.loadAllExternalProjects();
             if (closed) {
                 return;
             }
